472227e7083c40e0f924960fa105e7b94b12b18c
Yonatan Abramovich <yonatan.abramovich@post.runi.ac.il>
2023-12-24 09:09:00 +0200

    Add HW 1

--- Coins.java
+++ Coins.java
@@ -5,5 +5,12 @@
 public class Coins {
 	public static void main(String[] args) {
 		// Put your code here
+		int coins= Integer.parseInt(args[0]);
+		int quarters= coins /25;
+		int cents= coins % 25;
+		System.out.println("you need- "+ quarters+" quarters and "+cents+"cents");
+
+
+		//Use 1 quarters and 3 cents
 	}
 }
