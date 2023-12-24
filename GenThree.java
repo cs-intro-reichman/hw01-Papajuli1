8ededb782178cdf533479774fb082c4d689da95f
Yonatan Abramovich <yonatan.abramovich@post.runi.ac.il>
2023-12-24 10:27:19 +0200

    Add code for HW1

--- GenThree.java
+++ GenThree.java
@@ -7,13 +7,14 @@ public class GenThree {
 		int a= Integer.parseInt(args[0]);
 		int b= Integer.parseInt(args[1]);
 
-		int num1= Math.random(a,b);	
-		int num2= Math.random(a,b);	
-		int num3= Math.random(a,b);	
-		int numMin= Math.min(num1,num2,num3);
+		int num1= (int) (Math.random()*(b-a))+a;	
+		int num2= (int) (Math.random()*(b-a))+a;
+		int num3= (int) (Math.random()*(b-a))+a;
+
+		int min= (int) ((Math.min(Math.min(num1,num2),num3)));
 		System.out.println(num1);
 		System.out.println(num2);
 		System.out.println(num3);
-		System.out.println("The minimal generated number was"+numMin);
+		System.out.println("The minimal generated number was "+min);
 	}
 }
