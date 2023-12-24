ab32a60bd70070ed91963b82121d559480b842a6
Yonatan Abramovich <yonatan.abramovich@post.runi.ac.il>
2023-12-24 11:25:05 +0200

    A

--- AddTwo.java
+++ AddTwo.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int result = a + b;
-		System.out.printl(a+" + "+b+" = "+result);
+		System.out.printl(a + " + " + b + " = " + result);
 	}
 }
