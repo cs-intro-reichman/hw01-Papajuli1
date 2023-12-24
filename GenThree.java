3125f533b784b7f1ecf2dddc156354185de5203c
Yonatan Abramovich <yonatan.abramovich@post.runi.ac.il>
2023-12-24 11:22:07 +0200

    The Real Final Add

--- GenThree.java
+++ GenThree.java
@@ -4,15 +4,15 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		int a= Integer.parseInt(args[0]);
-		int b= Integer.parseInt(args[1]);
-		int num1= (int) (Math.random()*(b-a))+a;	
-		int num2= (int) (Math.random()*(b-a))+a;
-		int num3= (int) (Math.random()*(b-a))+a;
-		int min= (int) ((Math.min(Math.min(num1,num2),num3)));
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int num1 = (int) (Math.random() * (b - a) ) + a;	
+		int num2 = (int) (Math.random() * (b - a) ) + a;
+		int num3 = (int) (Math.random() * (b - a) ) + a;
+		int min = (int) ((Math.min(Math.min(num1,num2),num3)));
 		System.out.println(num1);
 		System.out.println(num2);
 		System.out.println(num3);
-		System.out.println("The minimal generated number was "+min);
+		System.out.println("The minimal generated number was " + min);
 	}
 }
