8be9678beef90cdf4b0c7c28a099bddffecf830f
Yonatan Abramovich <yonatan.abramovich@post.runi.ac.il>
2023-12-24 10:02:19 +0200

    Add cod for HW1

--- Coins.java
+++ Coins.java
@@ -4,13 +4,13 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		
 		int coins= Integer.parseInt(args[0]);
 		int quarters= coins /25;
 		int cents= coins % 25;
-		System.out.println("you need- "+ quarters+" quarters and "+cents+"cents");
+		System.out.println("Use "+quarters+" quarters and "+cents+" cents");
 
 
-		//Use 1 quarters and 3 cents
+		
 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,16 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a= Integer.parseInt(args[0]);
+		int b= Integer.parseInt(args[1]);
+
+		int num1= Math.random(a,b);	
+		int num2= Math.random(a,b);	
+		int num3= Math.random(a,b);	
+		int numMin= Math.min(num1,num2,num3);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println("The minimal generated number was"+numMin);
 	}
 }
